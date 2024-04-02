feeff20672bea42f2347077f7ed9987d37819955
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2024-04-02 13:41:19 +0300

    micro fixes

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Provider store={store}>
+      <Provider store={store}>
+        <BrowserRouter>
           <App />
-        </Provider>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
