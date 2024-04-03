eb48b3c64423eeda8738a06c529222286662ccd1
Tim <37597659+Aivazov@users.noreply.github.com>
2024-04-03 13:52:04 +0300

    fixing error with render before taking data from back

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -25,21 +25,21 @@ export const FullPost = () => {
       });
     }, [id]);
     
-    console.log('data Fullpost.jsx', data);
+    // console.log('data Fullpost.jsx', data);
   if (isLoading) {
     return <Post isLoading={isLoading} />;
   }
 
   return (
     <>
-      <Post
-          _id={data.id}
+      {data && <Post
+          id={data.id}
           title={data.title}
-          imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+          imageUrl={data.imageUrl}
           user={{
             avatarUrl:
               'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-            fullName: data.author.fullName,
+            fullName: data.author,
           }}
           createdAt={data.createdAt}
           viewsCount={data.viewsCount}
@@ -51,7 +51,7 @@ export const FullPost = () => {
           <p>{data.text}</p>
         {/* id={1}
         title="Roast the code #1 | Rock Paper Scissors"
-        imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+        imageUrl="https://res.cloudinary.scom/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
         user={{
           avatarUrl:
             'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
@@ -69,8 +69,8 @@ export const FullPost = () => {
           much more to it, just be polite and constructive, this is an exercise
           so we can all learn together. Now then, head over to the repo and
           roast as hard as you can!!
-        </p> */}
-      </Post>
+        </p>  */}
+      </Post>}
       <CommentsBlock
         items={[
           {
