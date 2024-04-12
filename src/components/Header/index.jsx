ec3cd3738074bb2afbaf0554a084cfd1adf58828
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2024-04-12 21:15:14 +0300

    .

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
       <Container maxWidth="lg">
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-            <div>AIVAZOV BLOG</div>
+            <div>AIVAZOV TEST BLOG</div>
           </Link>
           <div className={styles.buttons}>
             {isAuth ? (
